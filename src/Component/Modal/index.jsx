dc52aaf29b7c1ca98dba1a5adbd5a60830122da9
EmilyRacheal <emilychealy4@gmail.com>
2022-08-05 14:34:23 +0100

    styling the modal

--- src/Component/Modal/index.jsx
+++ src/Component/Modal/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./modal.module.css";
-import { AiOutlinePlus } from "react-icons/ai";
+import { FaTimes } from "react-icons/fa";
 
 function Index() {
   return (
     <div>
-      <div className="flex items-center justify-center"></div>
-      <div className="p-5 w-1/2 flex">
-        <div className="flex justify-end">
-          <AiOutlinePlus />
+      <div className="flex items-center justify-center">
+        <div className="p-5 flex bg-red-200   w-1/2">
+          <div className="flex justify-end w-full">
+            <FaTimes />
+          </div>
         </div>
       </div>
     </div>
