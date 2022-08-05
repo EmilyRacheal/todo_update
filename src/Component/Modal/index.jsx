382418e880d4f3a442ca09cdb62dfdd10a82b745
EmilyRacheal <emilychealy4@gmail.com>
2022-08-05 14:22:13 +0100

    create a modal component

--- src/Component/Modal/index.jsx
+++ src/Component/Modal/index.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styles from "./modal.module.css";
+import { AiOutlinePlus } from "react-icons/ai";
 
 function Index() {
-  return <div>Index</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-center"></div>
+      <div className="p-5 w-1/2 flex">
+        <div className="flex justify-end">
+          <AiOutlinePlus />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Index;
