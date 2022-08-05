014b44ad6229ce52ef8cbe2b28f51d43240bbf29
EmilyRacheal <emilychealy4@gmail.com>
2022-08-05 14:50:56 +0100

    styling form input

--- src/Component/Modal/index.jsx
+++ src/Component/Modal/index.jsx
@@ -6,10 +6,22 @@ function Index() {
   return (
     <div>
       <div className="flex items-center justify-center">
-        <div className="p-5 flex bg-red-200   w-1/2">
+        <div className="p-5  bg-red-200   w-1/2">
           <div className="flex justify-end w-full">
             <FaTimes />
           </div>
+          <form action="" className="flex flex-col">
+            <label htmlFor="" className="font-bold py-3">
+              Add Task
+            </label>
+            <input type="text" className={`${styles.textInput} py-2 px-3`} />
+            <label htmlFor="" className="font-bold py-3">
+              Choose Task Category
+            </label>
+            <select name="" id="">
+              <option value="">Learning</option>
+            </select>
+          </form>
         </div>
       </div>
     </div>
--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -14,7 +14,7 @@ function Index() {
       <div className="px-10 py-10">
         <div className={`${styles.heroBg} w-full py-5`}>
           <h4 className="text-lg text-white">Hello</h4>
-          <h1 className="text-2xl text-white font-bold">Guidotti Emily</h1>
+          <h1 className="text-2xl text-white font-bold">Emily Guidotti </h1>
         </div>
         <form action="">
           <div className={`${styles.textInput} flex items-center py-2 px-3`}>
