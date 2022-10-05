5f82005ead6f166333474f630fae5e6fc720731d
EmilyRacheal <emilychealy4@gmail.com>
2022-10-05 15:38:21 +0100

    setting the value of the input in the bos modal

--- src/Component/Modal/index.jsx
+++ src/Component/Modal/index.jsx
@@ -4,6 +4,17 @@ import { FaTimes } from "react-icons/fa";
 import { useState } from "react";
 
 function Index({ toggle }) {
+  const [taskInput, setTaskInput] = useState("king");
+  const [CategoryTask, setCategoryTask] = useState("you");
+  const handleTaskInput = (e) => {
+    setTaskInput(e.target.value);
+  };
+  console.log(taskInput);
+  const handleCategoryInput = (e) => {
+    setTaskInput(e.target.value);
+  };
+  console.log(CategoryTask);
+
   return (
     <div className="flex items-center justify-center w-full">
       <div className={`${styles.box} px-5 pt-5 pb-10  bg-red-200  `}>
@@ -14,15 +25,24 @@ function Index({ toggle }) {
           <label htmlFor="" className="font-bold py-3">
             Input Task
           </label>
-          <input type="text" className={`${styles.textInput} py-2 px-3`} />
+          <input
+            type="text"
+            onChange={handleTaskInput}
+            className={`${styles.textInput} py-2 px-3`}
+          />
           <label htmlFor="" className="font-bold py-3">
             Choose Task Category
           </label>
-          <select name="" id="" className={`${styles.textInput} py-2 px-3`}>
-            <option value="Learning">Learning</option>
-            <option value="Learning">Learning</option>
-            <option value="Learning">Learning</option>
-            <option value="Learning">Learning</option>
+          <select
+            name=""
+            id=""
+            onChange={handleCategoryInput}
+            className={`${styles.textInput} py-2 px-3`}
+          >
+            <option value="Studying">Studying</option>
+            <option value="Meditating">Meditating</option>
+            <option value="Designing">Designing</option>
+            <option value="Coding">Coding</option>
           </select>
         </form>
         <button className={`${styles.btn} text-sm px-4 py-3 mt-7`}>
